aa7157d4cefc0bae7eb363f07164f2bddc0dc9a0
shahqufi <shahqufi@gmail.com>
2025-03-24 00:34:20 +0530

    Update device.mk to change post-install script and add new packages for A/B OTA support

--- device.mk
+++ device.mk
@@ -33,7 +33,7 @@ PRODUCT_PACKAGES += \
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/mtk_plpath_utils \
+    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
     FILESYSTEM_TYPE_system=erofs \
     POSTINSTALL_OPTIONAL_system=true
 
@@ -42,6 +42,13 @@ AB_OTA_POSTINSTALL_CONFIG += \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
     FILESYSTEM_TYPE_vendor=erofs \
     POSTINSTALL_OPTIONAL_vendor=true
+
+PRODUCT_PACKAGES += \
+    update_engine \
+    update_engine_sideload \
+    update_verifier \
+    checkpoint_gc \
+    otapreopt_script
 
 #TODO : is this enough?
 # Bootctrl
