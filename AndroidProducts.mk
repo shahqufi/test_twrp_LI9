577f983ed3879c19575c4121038878151d2685d4
CHIRO <90218247+shahqufi@users.noreply.github.com>
2026-07-19 04:52:53 +0530

    Update copyright and product makefiles in AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,12 +1,18 @@
 #
-# Copyright (C) 2026 OrangeFox Recovery Project
-# SPDX-License-Identifier: Apache-2.0
+# Copyright 2017 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/twrp_LI9.mk
-
-COMMON_LUNCH_CHOICES := \
-    twrp_LI9-user \
-    twrp_LI9-userdebug \
-    twrp_LI9-eng
+    $(LOCAL_DIR)/ofox_LI9.mk
