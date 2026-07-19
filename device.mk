3c50db9bca56aa98d2f02e5445bc83302c898a7f
CHIRO <90218247+shahqufi@users.noreply.github.com>
2026-07-20 01:30:03 +0530

    Comment out package and post-install configurations
    Comment out PRODUCT_PACKAGES and AB_OTA_POSTINSTALL_CONFIG sections in device.mk.

--- device.mk
+++ device.mk
@@ -39,30 +39,29 @@ AB_OTA_PARTITIONS += \
     vbmeta_vendor \
     system \
     system_ext \
+    system_dlkm \
     product \
     vendor \
-    vendor_dlkm \
-    system_dlkm \
-    odm_dlkm
+    vendor_dlkm
 
-PRODUCT_PACKAGES += \
-    update_engine \
-    update_engine_sideload \
-    update_verifier \
-    checkpoint_gc \
-    otapreopt_script
+#PRODUCT_PACKAGES += \
+#    update_engine \
+#    update_engine_sideload \
+#    update_verifier \
+#    checkpoint_gc \
+#    otapreopt_script
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
-    POSTINSTALL_OPTIONAL_system=true
+#AB_OTA_POSTINSTALL_CONFIG += \
+#    RUN_POSTINSTALL_system=true \
+#    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
+#    FILESYSTEM_TYPE_system=erofs \
+#    POSTINSTALL_OPTIONAL_system=true
 
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_vendor=true \
-    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
-    POSTINSTALL_OPTIONAL_vendor=true
+#AB_OTA_POSTINSTALL_CONFIG += \
+#    RUN_POSTINSTALL_vendor=true \
+#    POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
+#    FILESYSTEM_TYPE_vendor=erofs \
+#    POSTINSTALL_OPTIONAL_vendor=true
 
 # -----------------------------------------------------------------------------
 # Boot Control HAL
